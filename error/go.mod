7516bde2a6cd7b7cc5e01a9a430e667d7a125d79
startstorm <imchenjian@firefox.com>
2021-04-24 17:11:18 +0800

    error

--- error/go.mod
+++ error/go.mod
@@ -1,5 +1,8 @@
-module geekband/error
+module learn/error
 
 go 1.15
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/pkg/errors v0.9.1
+)
--- error/main.go
+++ error/main.go
@@ -1,22 +1,46 @@
+/*
+*	因为错误在DAO层，不在业务层，而且该错误只是查询数据没有找到，不算是个错误
+*	该错误应该降级处理，返回nil
+*/
 package main
 
 import (
-	E "errors"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
+	"log"
 )
 
-func main() {
-	err0 := t1()
-	err := errors.Wrap(err0, "附加信息")
+func test() error {
+	var username string
+	// 查询不存在的一行
+	err := db.QueryRow("select username from sys_users where id = ?", 4).Scan(&username)
 	if err != nil {
-		//打印错误需要%+v才能详细输出
-		fmt.Printf("err :%+v\n", err)
+		if err == sql.ErrNoRows{
+			// 处理错误
+			log.Fatal("sql.ErrNoRows", err)
+			return nil
+		}else{
+			return errors.Wrap(err, "no sql.ErrNoRows err")
+		}
 	}
-
-	fmt.Println("Hello world")
+	return nil
 }
 
-func t1() error {
-	return E.New("错误")
+var db sql.DB
+
+func main() {
+	db, err := sql.Open("mysql", "root:c886114j@tcp(127.0.0.1:3306)/ginvueadmin")
+	err = db.Ping()
+	// 检查数据库是否连接成功
+	if err != nil {
+		fmt.Println("error")
+		log.Fatal(err)
+	}
+	fmt.Println("success")
+
+
+
+	defer db.Close()
 }
